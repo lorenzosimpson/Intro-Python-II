66fe5c86d7ab3f311824b96950c8cb5d94dc8b53
Lorenzo Simpson <lorenzosimp@gmail.com>
2020-02-11 15:55:03 -0500

    creates Player and Room classes

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,7 +39,8 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+player_1 = Player('Lorenzo', 'outside')
+print(player_1)
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +51,5 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+#def parse_cmd():
